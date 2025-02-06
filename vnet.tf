7a49f9489dd078644f73868620542e2e12a4647f
Steve Fibich <steve.fibich@nowhere.com>
2025-02-06 07:10:16 -0500

    rebooting repo changes

--- vnet.tf
+++ vnet.tf
@@ -1,26 +1,9 @@
-resource "azurerm_network_security_group" "example" {
-  name                = "example-security-group"
-  location            = azurerm_resource_group.vnet.location
-  resource_group_name = azurerm_resource_group.vnet.name
-}
-
-resource "azurerm_virtual_network" "example" {
+resource "azurerm_virtual_network" "primary" {
   name                = "example-network"
   location            = azurerm_resource_group.vnet.location
   resource_group_name = azurerm_resource_group.vnet.name
-  address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+  address_space       = ["10.30.0.0/16"]
 
-  subnet {
-    name             = "subnet1"
-    address_prefixes = ["10.0.1.0/24"]
-  }
-
-  subnet {
-    name             = "subnet2"
-    address_prefixes = ["10.0.2.0/24"]
-    security_group   = azurerm_network_security_group.example.id
-  }
 
   tags = local.main_tags
 }
