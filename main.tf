bdc758101da7e846aa401a9bfd51110e9f5b6adb
Steve Fibich <steve.fibich@nowhere.com>
2025-02-06 07:04:49 -0500

    vnet started

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 locals {
   main_tags        = merge(var.tags, { env = var.env, state_container_name = var.state_container_name, state_key = var.state_key })
-  rg_name          = "${var.prefix}-${var.env}-dbx"
-  name = replace(local.rg_name,"-","")
+  name          = "${var.prefix}-${var.env}"
+  name2 = replace(local.name,"-","")
 }
 
-resource "azurerm_resource_group" "main" {
-  name     = "${local.rg_name}-rg"
+resource "azurerm_resource_group" "vnet" {
+  name     = "${local.name}-vnet-rg"
   location = var.location
   tags     = local.main_tags
 }
