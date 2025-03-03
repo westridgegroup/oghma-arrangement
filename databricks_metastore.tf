eb7aac4a7ca01a2283b05f3ce253854e8b8ef271
Steve Fibich <steve.fibich@nowhere.com>
2025-03-03 07:23:34 -0500

    early commit

--- databricks_metastore.tf
+++ databricks_metastore.tf
@@ -1,16 +1,10 @@
-resource "databricks_metastore" "this" {
-  name = "primary"
-  storage_root = format("abfss://%s@%s.dfs.core.windows.net/",
-    azurerm_storage_container.metastore.name,
-  azurerm_storage_account.raw.name)
-  owner         = var.admin
-  region        = azurerm_resource_group.raw.location
-  force_destroy = true
+data "databricks_metastores" "all" {}
+
+output "all_metastores" {
+  value = data.databricks_metastores.all.ids
 }
 
-resource "azurerm_storage_account" "metastore" {
 
-}
 
 #resource "databricks_metastore_assignment" "this" {
 #  metastore_id = databricks_metastore.this.id
